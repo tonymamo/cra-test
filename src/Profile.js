d733c88f0636169a79064cf1e3368c31570d205c
Tony Mamo <tony.mamo@galileo-insights.com>
2019-12-06 11:06:38 -0500

    adding Profile.js

---
+++ src/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => (
+  <div>
+    <img src="https://avatars1.githubusercontent.com/u/1933657?s=460&v=4" alt="profile pic" />
+    <h1>Tony Mamo</h1>
+  </div>
+);
+
+export default Profile;
