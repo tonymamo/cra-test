615571f5bc5bd95f55ce6e144f349f1f057bb613
Tony Mamo <tony.mamo@galileo-insights.com>
2019-12-06 12:22:29 -0500

    map over data array to render a Profile for each person aka object in the array

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Profile imageSource={data[0].imgPath} first={data[0].firstName} last={data[0].lastName} />
-        <Profile imageSource={data[1].imgPath} first={data[1].firstName} last={data[1].lastName} />
-        <Profile imageSource={data[2].imgPath} first={data[2].firstName} last={data[2].lastName} />
+        {
+          data.map((person) => (
+            <Profile imageSource={person.imgPath} first={person.firstName} last={person.lastName} />
+          ))
+        }
       </header>
     </div>
   );
