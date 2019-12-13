3431541045b5ae4411cdeaa400bb58c4269773e8
Tony Mamo <tony.mamo@galileo-insights.com>
2019-12-13 12:09:12 -0500

    adding function to join together first and last name, and use template string in github profile link with username

--- src/Profile.js
+++ src/Profile.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+const createName = (person) => {
+  return `${person.firstName} ${person.lastName}`;
+}
+
 const Profile = ({ person }) => (
   <div className="profile-wrapper">
     <div className="profile-header">
       <img className="profile-pic" src={person.imgPath} alt="profile pic" />
-      <h1 className="profile-heading">{person.firstName} {person.lastName}</h1>
+      <h1 className="profile-heading">{createName(person)}</h1>
+      <p><a href={`https://github.com/${person.userName}`}>{person.userName}</a></p>
     </div>
     <ul className="profile-repositories">
       {
-        person.repositories.map((repo) => (
-          <li key={repo.url}><a className="profile-repo-link" href={repo.url}>{repo.name}</a></li>
+        person.repositories.map((item) => (
+          <li key={item.url}><a className="profile-repo-link" href={item.url}>{item.name}</a></li>
         ))
       }
     </ul>
