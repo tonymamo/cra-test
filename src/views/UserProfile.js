3cea64d5415e4e2586677b90e9724948faed0ce0
Tony Mamo <tony.mamo@galileo-insights.com>
2020-02-06 20:45:26 -0500

    make UserProfile responsive

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
 
 import Heading from '../components/Heading';
+import Row from '../components/Row';
+import Column from '../components/Column';
+
+import { halfSpacer, baseSpacer, quadrupleSpacer } from '../sizes';
+import { white, midGrey } from '../colors';
+
+const Avatar = styled.img`
+  width: ${quadrupleSpacer};
+  border-radius: ${quadrupleSpacer};
+`;
+
+const StyledRepoBox = styled.div`
+  background-color: ${white};
+  border: 1px solid ${midGrey};
+  margin-bottom: ${baseSpacer};
+  padding: ${baseSpacer};
+  border-radius: ${halfSpacer};
+`;
 
 function UserProfile(props) {
   // We can use the `useParams` hook here to access
@@ -14,8 +33,35 @@ function UserProfile(props) {
 
   return (
     <div>
-      {console.log(user)}
-      <Heading>{user.userName}</Heading>
+      <Row>
+        <Column md={4}>
+          <Avatar src={user.imgPath} />
+          <Heading>{user.firstName} {user.lastName}</Heading>
+          <p>{user.userName}</p>
+          <hr />
+          <p>{user.location}</p>
+          <p>{user.email}</p>
+          <p>{user.website}</p>
+        </Column>
+        <Column md={8}>
+          <Heading>Repositories</Heading>
+          {
+            user.repositories.map((repo) => (
+              <StyledRepoBox key={repo.url}>
+                <Row>
+                  <Column sm={9}>
+                    <p><a href={repo.url}>{repo.name}</a></p>
+                    <p>{repo.type} - {repo.lastUpdated}</p>
+                  </Column>
+                  <Column sm={3}>
+                    <p>{repo.stars} stars</p>
+                  </Column>
+                </Row>
+              </StyledRepoBox>
+            ))
+          }
+        </Column>
+      </Row>
     </div>
   );
 }
